4d5168a8cf6ae2d8fa8e0e60f061b24540975844
wjdgpwl1004 <wjdgpwl1004@naver.com>
2021-06-28 14:19:38 +0900

    configureStore 수정

--- store/configureStore.ts
+++ store/configureStore.ts
@@ -15,7 +15,7 @@ const configureStore = () => {
             applyMiddleware(...middlewares),
         );
     const store = createStore(reducer, enhancer);
-    store.sagaTask = sagaMiddleware.run(rootSaga);
+    (store as any).sagaTask = sagaMiddleware.run(rootSaga);
     return store;
 };
 
